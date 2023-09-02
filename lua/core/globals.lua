170275aa6aada5eee9d84ab7c5871045b312efd7
Oscar Wallberg <oscar.wallberg@outlook.com>
2023-09-03 01:04:04 +0200

    Clean up globals

--- lua/core/globals.lua
+++ lua/core/globals.lua
@@ -14,20 +14,6 @@
     limitations under the License.
 ]]
 
-vim.g.is_win = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
-vim.g.is_linux = vim.fn.has("unix") == 1 and vim.fn.has("win64") ~= 1
-vim.g.is_mac = vim.fn.has("macunix") == 1
-
-if vim.fn.executable("python3") then
-    if vim.g.is_win then
-        vim.g.python3_host_prog = string.gsub(vim.fn.exepath("python3"), ".exe$", "")
-    elseif vim.g.is_linux or vim.g.is_mac then
-        vim.g.python3_host_prog = vim.fn.exepath("python3")
-    end
-else
-    error("Python 3 executable not found! You must install Python 3 and set its PATH correctly!")
-end
-
 vim.g.mapleader = " "
 vim.g.vimsyn_embed = "1"
 vim.fn.execute("language en_US.utf-8")
