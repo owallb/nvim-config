4de37f92057c9ef457be558f340840c7600b853b
Oscar Wallberg <oscar.wallberg@outlook.com>
2023-09-04 15:35:06 +0200

    Clean up luasnip

--- lua/config/luasnip.lua
+++ lua/config/luasnip.lua
@@ -16,18 +16,4 @@
 
 -- https://github.com/L3MON4D3/LuaSnip
 
--- local ls = require"luasnip"
--- local s = ls.snippet
--- local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
--- local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
--- local events = require("luasnip.util.events")
--- local ai = require("luasnip.nodes.absolute_indexer")
-
--- For rafamadriz/friendly-snippets
 require("luasnip.loaders.from_vscode").lazy_load()
