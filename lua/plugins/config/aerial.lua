362e24e85a32584193ce097ebd890da7cd6fdfbe
Oscar Wallberg <oscar.wallberg@incoax.com>
2023-09-13 20:23:25 +0200

    Remove some aerial keybinds

--- lua/plugins/config/aerial.lua
+++ lua/plugins/config/aerial.lua
@@ -24,14 +24,7 @@ require("aerial").setup({
         unlisted_buffers = true,
     },
     on_attach = function (bufnr)
-        -- Toggle the aerial window with <leader>a
         vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>at", "<cmd>AerialToggle!<CR>", {})
-        -- Jump forwards/backwards with '{' and '}'
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "{", "<cmd>AerialPrev<CR>", {})
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "}", "<cmd>AerialNext<CR>", {})
-        -- Jump up the tree with '[[' or ']]'
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "[[", "<cmd>AerialPrevUp<CR>", {})
-        vim.api.nvim_buf_set_keymap(bufnr, "n", "]]", "<cmd>AerialNextUp<CR>", {})
     end,
     show_guides = true,
 })
