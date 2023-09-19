46609d416a6c0195d4c0eab4e2785e886776ad67
Oscar Wallberg <oscar.wallberg@incoax.com>
2023-09-20 01:19:48 +0200

    Revert "Remove nvim-tree"
    This reverts commit e5146fbaa4b222d128a338953d0557045a55df29.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -178,6 +178,10 @@ local plugins = {
         ft = { "jenkinsfile", "Jenkinsfile", },
     },
     {
+        "kyazdani42/nvim-tree.lua",
+        config = function () require("plugins.config.tree") end,
+    },
+    {
         "dstein64/vim-startuptime",
         lazy = true,
         event = "VimEnter",
