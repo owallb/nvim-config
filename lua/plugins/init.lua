74d6b1b5c95d3c5ea9c8dacb43b402418abc3466
Oscar Wallberg <oscar.wallberg@incoax.com>
2023-09-19 16:01:52 +0200

    Fix loading of notify

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,6 +29,7 @@ local plugins = {
     {
         "rcarriga/nvim-notify",
         priority = 900,
+        lazy = false,
         config = function () require("plugins.config.notify") end,
     },
     {
@@ -205,12 +206,7 @@ local plugins = {
         "folke/noice.nvim",
         event = "VeryLazy",
         dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
             "MunifTanjim/nui.nvim",
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            "rcarriga/nvim-notify",
         },
         config = function () require("plugins.config.noice") end,
     },
