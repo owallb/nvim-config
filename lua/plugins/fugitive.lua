d37268dff297950ab1bded904578f9f0a27a267f
Oscar Wallberg <oscar.wallberg@incoax.com>
2023-09-24 09:59:40 +0200

    Add git push keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -26,6 +26,7 @@ end
 vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit)
 vim.keymap.set("n", "<leader>gc", function () vim.cmd.G("commit") end)
 vim.keymap.set("n", "<leader>ga", function () vim.cmd.G("commit --amend") end)
+vim.keymap.set("n", "<leader>gp", function () vim.cmd.G("push") end)
 
 -- Only used if diffview is not available
 if not pcall(require, "diffview") then
