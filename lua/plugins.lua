cc663a8a42a3a0c4b71b83d48a19ac7a21325da4
Oscar Wallberg <oscar.wallberg@pm.me>
2023-12-23 01:51:03 +0100

    [telescope] Update config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -147,6 +147,13 @@ local plugins = {
         },
     },
     {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build =
+            "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release" ..
+            " && cmake --build build --config Release" ..
+            " && cmake --install build --prefix build",
+    },
+    {
         "numToStr/Comment.nvim",
         config = require("plugins.comment"),
         lazy = true,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,17 +17,44 @@
 -- https://github.com/nvim-telescope/telescope.nvim
 
 local function setup()
+    local telescope = require("telescope")
     local builtin = require("telescope.builtin")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+        pickers = {
+            buffers = {
+                mappings = {
+                    i = {
+                        ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
+                    },
+                },
+            },
+        },
+    })
 
     vim.keymap.set(
         "n",
         "<leader>ff",
-        function () builtin.find_files({ hidden = true, }) end
+        function ()
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true,
+                no_ignore_parent = true,
+                previewer = false,
+            })
+        end
     )
     vim.keymap.set(
         "n",
         "<leader>fr",
-        function () builtin.oldfiles({ hidden = true, }) end
+        function ()
+            builtin.oldfiles({
+                only_cwd = true,
+                hidden = true,
+                previewer = false,
+            })
+        end
     )
     vim.keymap.set(
         "n", "<leader>fg", function ()
@@ -40,10 +67,19 @@ local function setup()
                         "--iglob=!.git",
                     }
                 end,
+                previewer = false,
             })
         end
     )
-    vim.keymap.set("n", "<leader>fb", builtin.buffers)
+    vim.keymap.set(
+        "n",
+        "<leader>fb",
+        function ()
+            builtin.buffers({ previewer = false, })
+        end
+    )
+
+    telescope.load_extension("fzf")
 end
 
 return setup
