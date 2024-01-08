07dbf09489161b759503e8eb1301addfd74ca875
Oscar Wallberg <oscar.wallberg@pm.me>
2024-01-08 18:25:10 +0100

    feat: add grapple toggle keybind

--- lua/plugins/grapple.lua
+++ lua/plugins/grapple.lua
@@ -4,12 +4,32 @@ local function setup()
     local grapple = require("grapple")
     grapple.setup()
     vim.keymap.set("n", "<leader>'", grapple.popup_tags)
+    vim.keymap.set("n", "<leader>mm", function ()
+        if grapple.exists() then
+            grapple.untag()
+            return
+        end
+
+        for i = 1, 9 do
+            local opts = { key = "m" .. i, }
+            if not grapple.exists(opts) then
+                grapple.tag(opts)
+                return
+            end
+        end
+
+        grapple.tag({ key = "m?", })
+    end)
+
     for i = 1, 9 do
+        local opts = { key = "m" .. i, }
+
         vim.keymap.set("n", "<leader>m" .. i, function ()
-            grapple.tag({ key = "m" .. i, })
+            grapple.tag(opts)
         end)
+
         vim.keymap.set("n", "<leader>" .. i, function ()
-            grapple.select({ key = "m" .. i, })
+            grapple.select(opts)
         end)
     end
 end
