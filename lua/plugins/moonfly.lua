a36965f9e0f3df4761e0872e8ba482c28bc48b78
Oscar Wallberg <oscar.wallberg@pm.me>
2024-01-09 23:14:17 +0100

    fix(theme): restore theme default diff colors

--- lua/plugins/moonfly.lua
+++ lua/plugins/moonfly.lua
@@ -1,30 +1,9 @@
 -- https://github.com/bluz71/vim-moonfly-colors
 
-local utils = require("utils")
-
-local function override_hl(hl_name, opts)
-    local hl = vim.api.nvim_get_hl(0, { name = hl_name, })
-    utils.update_table(hl, opts)
-    vim.api.nvim_set_hl(0, hl_name, hl)
-end
-
 local function setup()
     vim.g.moonflyNormalFloat = true
     vim.g.moonflyCursorColor = true
     vim.g.moonflyWinSeparator = 2
-
-    local custom_highlight = vim.api.nvim_create_augroup(
-        "CustomMoonflyHighlight", {})
-    vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "moonfly",
-        callback = function ()
-            override_hl("DiffAdd", { bg = "#364143", })
-            override_hl("DiffText", { bg = "#3e4b6b", })
-            override_hl("DiffChange", { bg = "#25293c", })
-            override_hl("DiffDelete", { bg = "#443244", })
-        end,
-        group = custom_highlight,
-    })
 
     vim.cmd.colorscheme("moonfly")
 end
