8af5d7a71d29ebc3dbde996145bad06550feca55
Oscar Wallberg <oscar.wallberg@pm.me>
2024-02-26 19:09:36 +0100

    feat(zls): update config

--- lua/lsp/zls.lua
+++ lua/lsp/zls.lua
@@ -18,7 +18,11 @@ return {
         single_file_support = true,
         settings = {
             zls = {
+                enable_autofix = false,
                 enable_inlay_hints = false,
+                enable_build_on_save = true,
+                warn_style = true,
+                highlight_global_var_declarations = true,
             },
         },
     },
