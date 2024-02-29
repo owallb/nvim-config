32b71f2aaf5400446438ca16f13d21f5e203fadd
Oscar Wallberg <oscar.wallberg@pm.me>
2024-02-29 16:25:09 +0100

    fix(php): fix indentation for php

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,7 +21,12 @@ local function setup()
         highlight = {
             enable = true,
             disable = {},
-            additional_vim_regex_highlighting = { "org", },
+
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = { "org", "php" },
         },
     })
 
