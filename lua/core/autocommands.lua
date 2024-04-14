b3f1116a9b2c46b48cbf9185a3fa323227a2cdb4
Oscar Wallberg <oscar.wallberg@pm.me>
2024-04-14 15:57:34 +0200

    fix(moonfly): remove moonfly color override

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -4,12 +4,3 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.bo.expandtab = false
     end,
 })
-
-local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
-vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "moonfly",
-    callback = function ()
-        vim.api.nvim_set_hl(0, "@variable.member", { link = "MoonflyTurquoise", })
-    end,
-    group = custom_highlight,
-})
