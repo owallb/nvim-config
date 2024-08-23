20a7913539fa876504c1704ecfd6dfa52667b096
Oscar Wallberg <oscar.wallberg@pm.me>
2024-08-23 16:05:48 +0200

    feat(lemminx): change formatter to xmlformatter

--- lua/lsp/config/lemminx.lua
+++ lua/lsp/config/lemminx.lua
@@ -1,10 +1,40 @@
 -- spec: https://github.com/eclipse/lemminx/blob/main/docs/Configuration.md
 
+local utils = require("utils")
+
 return {
     enable = true,
     mason = {
         name = "lemminx",
-        -- version = "",
+        dependencies = { "xmlformatter" },
+    },
+    keymaps = {
+        {
+            mode = "n",
+            lhs = "<leader>lf",
+            rhs = function()
+                utils.format({
+                    cmd = {
+                        "xmlformat",
+                        "-",
+                    },
+                    output = "stdout",
+                })
+            end,
+        },
+        {
+            mode = "x",
+            lhs = "<leader>lf",
+            rhs = function()
+                utils.format({
+                    cmd = {
+                        "xmlformat",
+                        "-",
+                    },
+                    output = "stdout",
+                })
+            end,
+        },
     },
     lspconfig = {
         filetypes = {
@@ -20,7 +50,7 @@ return {
             settings = {
                 xml = {
                     format = {
-                        enabled = true, -- is able to format document
+                        enabled = false, -- is able to format document
                         splitAttributes = true, -- each attribute is formatted onto new line
                         joinCDATALines = false, -- normalize content inside CDATA
                         joinCommentLines = false, -- normalize content inside comments
