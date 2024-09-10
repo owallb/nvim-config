ac753858af5a85f26f445a5cc4e35d875a4fd474
Oscar Wallberg <oscar.wallberg@pm.me>
2024-09-10 08:37:23 +0200

    feat(oil): replace lf with oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,43 @@
+-- https://github.com/NvChad/nvim-colorizer.lua
+
+---@type LazyPluginSpec
+return {
+    "stevearc/oil.nvim",
+    keys = {
+        {
+            "<leader>fe",
+            function()
+                vim.cmd.Oil("--float")
+            end,
+            mode = "n",
+        },
+    },
+    opts = {
+        default_file_explorer = true,
+        columns = {
+            -- "icon",
+            "permissions",
+            "size",
+            "mtime",
+        },
+        constrain_cursor = "name",
+        delete_to_trash = true,
+        float = {
+            padding = 10,
+        },
+        skip_confirm_for_simple_edits = true,
+        watch_for_changes = false,
+        keymaps = {
+            ["q"] = "actions.close",
+            ["<C-s>"] = false,
+            ["<C-l>"] = false,
+            ["<C-r>"] = "actions.refresh",
+            ["<S-h>"] = "actions.parent",
+            ["<S-l>"] = "actions.select",
+        },
+        view_options = {
+            show_hidden = true,
+            natural_order = false,
+        },
+    },
+}
