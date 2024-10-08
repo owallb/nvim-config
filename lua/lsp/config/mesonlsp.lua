3aeed43dd445e5e011b2544c29dc99222e46c21e
Oscar Wallberg <oscar.wallberg@pm.me>
2024-10-08 21:02:59 +0200

    fix(mesonlsp): use correct root dir

--- lua/lsp/config/mesonlsp.lua
+++ lua/lsp/config/mesonlsp.lua
@@ -3,6 +3,15 @@ return {
     enable = true,
     mason = { "mesonlsp" },
     lspconfig = {
+        ---@type fun(filename: string, bufnr: number): string?
+        root_dir = function(filename, bufnr)
+            local parent = require("lspconfig").util.find_git_ancestor(filename)
+            if not parent then
+                local win = vim.fn.bufwinid(bufnr)
+                parent = vim.fn.getcwd(win)
+            end
+            return parent
+        end,
         settings = {
             others = {
                 disableInlayHints = true,
