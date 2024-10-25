2793b3c03c90030137f27caba8217dfa4a4a3982
Oscar Wallberg <oscar.wallberg@pm.me>
2024-10-25 13:32:29 +0200

    fix(lemminx): enable default formatter

--- lua/lsp/config/lemminx.lua
+++ lua/lsp/config/lemminx.lua
@@ -8,34 +8,6 @@ return {
         name = "lemminx",
         dependencies = { "xmlformatter" },
     },
-    keymaps = {
-        {
-            mode = "n",
-            lhs = "<leader>lf",
-            rhs = function()
-                utils.format({
-                    cmd = {
-                        "xmlformat",
-                        "-",
-                    },
-                    output = "stdout",
-                })
-            end,
-        },
-        {
-            mode = "x",
-            lhs = "<leader>lf",
-            rhs = function()
-                utils.format({
-                    cmd = {
-                        "xmlformat",
-                        "-",
-                    },
-                    output = "stdout",
-                })
-            end,
-        },
-    },
     lspconfig = {
         filetypes = {
             "xml",
@@ -50,7 +22,7 @@ return {
             settings = {
                 xml = {
                     format = {
-                        enabled = false, -- is able to format document
+                        enabled = true, -- is able to format document
                         splitAttributes = true, -- each attribute is formatted onto new line
                         joinCDATALines = false, -- normalize content inside CDATA
                         joinCommentLines = false, -- normalize content inside comments
