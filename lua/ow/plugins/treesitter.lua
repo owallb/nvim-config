96bcf880e1ada0db39c6b254eba66f61941cb9fc
Oscar Wallberg <oscar.wallberg@pm.me>
2025-04-20 21:12:32 +0200

    fix: disable treesitter highlightig for docker files

--- lua/ow/plugins/treesitter.lua
+++ lua/ow/plugins/treesitter.lua
@@ -19,7 +19,7 @@ return {
         auto_install = true,
         highlight = {
             enable = true,
-            disable = {},
+            disable = { "dockerfile" },
 
             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
