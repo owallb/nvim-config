aa37011ba07cc9433b9efedc3da7800e029b26c3
Oscar Wallberg <oscar.wallberg@pm.me>
2025-05-02 23:37:00 +0200

    fix(onedark): disable EndOfBuffer highlight

--- lua/ow/plugins/onedark.lua
+++ lua/ow/plugins/onedark.lua
@@ -10,7 +10,7 @@ return {
             NormalFloat = { bg = c.bg0 },
             FloatBorder = { bg = c.bg0 },
             TabLineSel = { fg = c.fg, bg = c.bg0 },
-            EndOfBuffer = nil,
+            EndOfBuffer = { fg = "NONE", bg = "NONE" },
             NvimTreeIndentMarker = { fg = c.bg3 },
         }
         require("onedark").set_options("highlights", highlights)
