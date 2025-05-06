7c6ccf75ee8346bc3418e32f0daace2023987f9d
Oscar Wallberg <oscar.wallberg@pm.me>
2025-05-06 09:18:00 +0200

    feat(telescope): add undo extension

--- lua/ow/plugins/telescope.lua
+++ lua/ow/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     event = "VeryLazy",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        "debugloop/telescope-undo.nvim",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "make",
@@ -26,6 +27,21 @@ return {
                     },
                 },
             },
+            extensions = {
+                undo = {
+                    mappings = {
+                        i = {
+                            ["<cr>"] = require("telescope-undo.actions").yank_deletions,
+                            ["<C-cr>"] = false,
+                            ["<C-r>"] = false,
+                            ["<C-y>"] = require("telescope-undo.actions").yank_additions,
+                        },
+                        n = {
+                            ["u"] = false,
+                        },
+                    },
+                },
+            },
             pickers = {
                 oldfiles = {
                     initial_mode = "normal",
@@ -34,10 +50,12 @@ return {
                     initial_mode = "normal",
                     mappings = {
                         n = {
-                            ["<C-d>"] = actions.delete_buffer + actions.move_to_top,
+                            ["<C-d>"] = actions.delete_buffer
+                                + actions.move_to_top,
                         },
                         i = {
-                            ["<C-d>"] = actions.delete_buffer + actions.move_to_top,
+                            ["<C-d>"] = actions.delete_buffer
+                                + actions.move_to_top,
                         },
                     },
                 },
@@ -62,65 +80,48 @@ return {
             },
         })
 
-        vim.keymap.set(
-            "n",
-            "<leader>ff",
-            function()
-                builtin.find_files({
-                    hidden = true,
-                    no_ignore = true,
-                    no_ignore_parent = true,
-                    previewer = false,
-                })
-            end
-        )
-        vim.keymap.set(
-            "n",
-            "<leader>fr",
-            function()
-                builtin.oldfiles({
-                    only_cwd = true,
-                    hidden = true,
-                    previewer = false,
-                })
-            end
-        )
-        vim.keymap.set(
-            "n",
-            "<leader>fg",
-            function()
-                builtin.live_grep({
-                    additional_args = function(_)
-                        return {
-                            "--hidden",
-                            "--iglob=!.venv",
-                            "--iglob=!vendor",
-                            "--iglob=!.git",
-                        }
-                    end,
-                    previewer = true,
-                })
-            end
-        )
-        vim.keymap.set(
-            "n",
-            "<leader>fb",
-            function()
-                builtin.buffers({ previewer = false, sort_mru = true })
-            end
-        )
-        vim.keymap.set(
-            "n",
-            "<leader>fd",
-            function()
-                builtin.diagnostics({
-                    bufnr = 0,
-                })
-            end
-        )
+        vim.keymap.set("n", "<leader>ff", function()
+            builtin.find_files({
+                hidden = true,
+                no_ignore = true,
+                no_ignore_parent = true,
+                previewer = false,
+            })
+        end)
+        vim.keymap.set("n", "<leader>fr", function()
+            builtin.oldfiles({
+                only_cwd = true,
+                hidden = true,
+                previewer = false,
+            })
+        end)
+        vim.keymap.set("n", "<leader>fg", function()
+            builtin.live_grep({
+                additional_args = function(_)
+                    return {
+                        "--hidden",
+                        "--iglob=!.venv",
+                        "--iglob=!vendor",
+                        "--iglob=!.git",
+                    }
+                end,
+                previewer = true,
+            })
+        end)
+        vim.keymap.set("n", "<leader>fb", function()
+            builtin.buffers({ previewer = false, sort_mru = true })
+        end)
+        vim.keymap.set("n", "<leader>fd", function()
+            builtin.diagnostics({
+                bufnr = 0,
+            })
+        end)
 
         telescope.load_extension("fzf")
         telescope.load_extension("notify")
         vim.keymap.set("n", "<leader>fn", telescope.extensions.notify.notify)
+
+        telescope.load_extension("undo")
+        vim.keymap.set("n", "<leader>fu", telescope.extensions.undo.undo)
     end,
 }
