1666ffa75a15cee1925e42cdfcac13c56310d1a6
Oscar Wallberg <oscar.wallberg@pm.me>
2025-05-21 00:25:10 +0200

    feat(pyright): add stubs path

--- lua/ow/lsp/config/pyright.lua
+++ lua/ow/lsp/config/pyright.lua
@@ -16,6 +16,7 @@ return {
                     diagnosticMode = "openFilesOnly",
                     useLibraryCodeForTypes = true,
                     typeCheckingMode = "strict",
+                    stubPath = "stubs",
                 },
             },
             pyright = {
