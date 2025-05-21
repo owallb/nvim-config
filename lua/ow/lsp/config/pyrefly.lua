32ca54d5b931d90c8fc079d7499f825dadc66ca7
Oscar Wallberg <oscar.wallberg@pm.me>
2025-05-21 06:44:46 +0200

    feat(lsp): replace jedi+pyright with pyrefly+ruff

---
+++ lua/ow/lsp/config/pyrefly.lua
@@ -0,0 +1,19 @@
+local util = require("lspconfig.util")
+
+---@type ServerConfig
+return {
+    dependencies = { "pyrefly" },
+    lspconfig = {
+        cmd = { "pyrefly", "lsp" },
+        filetypes = { "python" },
+        root_dir = util.root_pattern(
+            "pyrefly.toml",
+            "pyproject.toml",
+            "setup.py",
+            "setup.cfg",
+            "requirements.txt",
+            "Pipfile",
+            ".git"
+        ),
+    },
+}
